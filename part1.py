9b85bbe89a37fb437b8e8971031c8a5b740d1803
TruttimusRex <67364461+TruttimusRex@users.noreply.github.com>
2021-12-12 11:09:22 +0300

    Update part1.py

--- part1.py
+++ part1.py
@@ -4,5 +4,5 @@ import requests
 URL = "https://en.wikipedia.org/wiki/Donald_Trump"
 r = requests.get(URL)
 
-soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib
+soup = BeautifulSoup(r.content, 'html5lib') 
 print(soup.prettify())
